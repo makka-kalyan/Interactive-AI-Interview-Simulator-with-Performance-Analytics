518ba30327e4dec1c3eadf4c6a3ba6babe5399b4
Kalyan <kalyanmakka27@gmail.com>
2026-04-17 16:59:26 +0530

    Initialized Footer

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,6 +22,7 @@ import evalImg from "../assets/ai-ans.png";
 import resumeImg from "../assets/resume.png";
 import pdfImg from "../assets/pdf.png";
 import analyticsImg from "../assets/history.png";
+import Footer from "../components/Footer";
 
 function Home() {
   const { userData } = useSelector((state) => state.user);
@@ -289,6 +290,7 @@ function Home() {
       </div>
 
       {showAuth && <AuthModel onClose={() => setShowAuth(false)} />}
+      <Footer />
     </div>
   );
 }
