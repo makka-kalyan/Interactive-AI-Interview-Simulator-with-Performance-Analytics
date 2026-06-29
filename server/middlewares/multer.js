d226cccdbdaf7c02a0ac46139398d6c4c703a946
makka-kalyan <kalyanmakka27@gmail.com>
2026-06-29 18:28:50 +0530

    Add AI interview backend flow

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -1,16 +1,15 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: (req, file, cb) => {
     cb(null, "public");
   },
-  filename: function (req, file, cb) {
+  filename: (req, file, cb) => {
     const filename = Date.now() + "-" + file.originalname;
     cb(null, filename);
   },
 });
 
-export const upload = multer({
-  storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, //5MB limit
-});
+const upload = multer({ storage, limits: { fileSize: 5 * 1024 * 1024 } }); // 5MB limit
+
+export default upload;
--- server/services/openRouter.service.js
+++ server/services/openRouter.service.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+
 export const askAi = async (messages) => {
   try {
-    if (!messages || Array.isArray(messages) || messages.length === 0) {
+    if (!messages || !Array.isArray(messages) || messages.length === 0) {
       throw new Error("Messages array is empty.");
     }
     const response = await axios.post(
@@ -13,18 +14,20 @@ export const askAi = async (messages) => {
       {
         headers: {
           Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
-
           "Content-Type": "application/json",
         },
       },
     );
-    const content = response?.data?.choices?.[0]?.messages?.content;
+
+    const content = response?.data?.choices?.[0]?.message?.content;
+
     if (!content || !content.trim()) {
-      throw new Error("AI returned empty.");
+      throw new Error("AI returned empty response.");
     }
+
     return content;
   } catch (error) {
-    console.error("openRouter Error:", error.response?.data || error.messages);
+    console.error("OpenRouter Error:", error.response?.data || error.message);
     throw new Error("OpenRouter API Error");
   }
 };
