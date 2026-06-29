4c919a50f426f8efed947721e8d6556a82eb88a2
makka-kalyan <kalyanmakka27@gmail.com>
2026-06-29 18:28:32 +0530

    Add frontend pages and interview flows

--- client/src/pages/InterviewPage.jsx
+++ client/src/pages/InterviewPage.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import Step1SetUp from "../components/Step1SetUp";
 import Step2Interview from "../components/Step2Interview";
 import Step3Report from "../components/Step3Report";
 
-const interviewPage = () => {
-  const [step, step] = useState(1);
+function InterviewPage() {
+  const [step, setStep] = useState(1);
   const [interviewData, setInterviewData] = useState(null);
+
   return (
-    <div className="min-h-screen bg-fray-50">
+    <div className="min-h-screen bg-gray-50">
       {step === 1 && (
         <Step1SetUp
           onStart={(data) => {
@@ -16,18 +18,20 @@ const interviewPage = () => {
           }}
         />
       )}
+
       {step === 2 && (
         <Step2Interview
           interviewData={interviewData}
           onFinish={(report) => {
             setInterviewData(report);
-            setStep(2);
+            setStep(3);
           }}
         />
       )}
+
       {step === 3 && <Step3Report report={interviewData} />}
     </div>
   );
-};
+}
 
-export default interviewPage;
+export default InterviewPage;
